96a06ff58e5903614bc053eb7e3464c614540a52
HP <HP@DESKTOP-2U2L68L>
2024-12-10 14:23:25 +0300

    Proje dosyası ekle.

---
+++ Ödev1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yas = Convert.ToInt32(textBox1.Text);
+            if(radioButton1.Checked==true)
+            {
+                if(yas>=18)
+                {
+                    label3.Text="Merhaba Beyefendi,hoş geldiniz! ";
+                }
+                else
+                {
+                    label3.Text = "Yaşınız Küçük,Erişim İzniniz Yok.";
+                }
+            }
+           
+
+            if (radioButton2.Checked==true)
+            {
+                if(yas>=18)
+                {
+                    label3.Text = "Merhaba Hanımefendi,hoş geldiniz!";
+                }
+                else
+                {
+                    label3.Text = "Yaşınız Küçük,Erişim İzniniz Yok.";
+                }
+            }
+          
+         
+
+        }
+    }
+}
